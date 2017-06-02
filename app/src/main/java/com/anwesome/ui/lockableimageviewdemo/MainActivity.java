6c204abc02e367d5cc24accbead166ef89996cf0
Anwesh43 <anweshthecool0@gmail.com>
2017-06-02 15:32:34 +0530

    UsingLockImageView in app

--- app/src/main/java/com/anwesome/ui/lockableimageviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/lockableimageviewdemo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.anwesome.ui.lockableimageviewdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.lockableimageview.LockbaleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
+        LockbaleImageView lockbaleImageView = new LockbaleImageView(this,bitmap);
+        addContentView(lockbaleImageView,new ViewGroup.LayoutParams(1080,1080));
     }
 }
